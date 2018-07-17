c9f2aab9aa90b31cdf38a64e8737b672f6e46afb
xiaofengyangliu <970360490@qq.com>
2018-07-17 16:09:09 +0800

    添加jackson 转换方法

--- src/main/java/xzs/util/JacksonUtils.java
+++ src/main/java/xzs/util/JacksonUtils.java
@@ -4,4 +4,30 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JacksonUtils {
 	public static ObjectMapper mapper = new ObjectMapper();
+	/**
+	 *
+	 * @param object
+	 * @return
+	 */
+	public static String bean2Json(Object object){
+		String jsonStr="";
+		try {
+			jsonStr = mapper.writeValueAsString(object);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return jsonStr;
+	}
+
+	public static Object json2Bean(String jsonStr,Class<?> c){
+		Object o = null ;
+		try {
+			o =mapper.readValue(jsonStr, c);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return o;
+
+	}
 }
